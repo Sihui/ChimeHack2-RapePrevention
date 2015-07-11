c24e8a5d2c567d5c3c06676487cf311f476233e0
SiYuan Ma <siyuanma@squareup.com>
2015-07-11 16:35:02 -0700

    Create user name input activity

--- Rapeprevention/app/src/main/java/hackmate/rapeprevention/Controller/Controller.java
+++ Rapeprevention/app/src/main/java/hackmate/rapeprevention/Controller/Controller.java
@@ -1,6 +1,8 @@
 package hackmate.rapeprevention.Controller;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 
 public class Controller<T extends Activity> {
   Object activity;
@@ -22,5 +24,14 @@ public class Controller<T extends Activity> {
 
   public boolean hasActivity() {
     return activity != null;
+  }
+
+  public Context getContext() {
+    return getActivity();
+  }
+
+  public void startActivity(Class<? extends Activity> clazz) {
+    Intent intent = new Intent(getActivity(), clazz);
+    getActivity().startActivity(intent);
   }
 }
