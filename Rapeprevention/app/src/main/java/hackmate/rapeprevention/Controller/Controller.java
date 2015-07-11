79204489e8afc93a6bd9590ae05930c003a33721
SiYuan Ma <siyuanma@squareup.com>
2015-07-11 11:38:06 -0700

    Create reaction activity

--- Rapeprevention/app/src/main/java/hackmate/rapeprevention/Controller/Controller.java
+++ Rapeprevention/app/src/main/java/hackmate/rapeprevention/Controller/Controller.java
@@ -1,4 +1,26 @@
 package hackmate.rapeprevention.Controller;
 
-public class Controller {
+import android.app.Activity;
+
+public class Controller<T extends Activity> {
+  Object activity;
+
+  /** Get activity attached to the controller */
+  protected T getActivity() {
+    return (T) activity;
+  }
+
+  /** Make controller control the activity */
+  public void takeActivity(T activity) {
+    this.activity = activity;
+  }
+
+  /** Drop activity */
+  public void dropActivity() {
+    this.activity = null;
+  }
+
+  public boolean hasActivity() {
+    return activity != null;
+  }
 }
---
+++ Rapeprevention/app/src/main/java/hackmate/rapeprevention/Controller/ReactionController.java
@@ -0,0 +1,40 @@
+package hackmate.rapeprevention.Controller;
+
+import android.os.Handler;
+import hackmate.rapeprevention.ReactionActivity;
+import java.util.Date;
+import java.util.Random;
+
+public class ReactionController extends Controller<ReactionActivity> {
+  long startTime;
+  long changeColorTime;
+
+  private ReactionController() {}
+  public static ReactionController ONLY = new ReactionController();
+
+  @Override public void takeActivity(ReactionActivity activity) {
+    super.takeActivity(activity);
+    startMeasureReaction();
+  }
+
+  public void startMeasureReaction() {
+    Random random = new Random();
+    changeColorTime = random.nextInt(1000) + 2000;
+    startTime = getCurrentTime();
+    Handler handler = getActivity().handler;
+    handler.postDelayed(new Runnable() {
+      @Override public void run() {
+        getActivity().changeSignalColor();
+      }
+    }, changeColorTime);
+  }
+
+  public void onUserClickButton() {
+    long timeDiff = Math.abs(startTime + changeColorTime - getCurrentTime());
+    getActivity().notifyReactionTime(timeDiff);
+  }
+
+  long getCurrentTime() {
+    return (new Date()).getTime();
+  }
+}
