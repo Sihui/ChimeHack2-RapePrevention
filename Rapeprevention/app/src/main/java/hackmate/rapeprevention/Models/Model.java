f27dd280e71934f6260a8941432ba33b7abc43a9
SiYuan Ma <siyuanma@squareup.com>
2015-07-11 11:52:12 -0700

    Create Model

--- Rapeprevention/app/src/main/java/hackmate/rapeprevention/Models/Model.java
+++ Rapeprevention/app/src/main/java/hackmate/rapeprevention/Models/Model.java
@@ -1,4 +1,17 @@
 package hackmate.rapeprevention.Models;
 
+import android.location.Location;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Model {
+  public Location userLocation;
+  public String userName;
+  public List<Integer> reactionTime = new ArrayList<>();
+
+  private Model() {}
+  private static Model ONLY = new Model();
+  public static Model getModel() {
+    return ONLY;
+  }
 }
