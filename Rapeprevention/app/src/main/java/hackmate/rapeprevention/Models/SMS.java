c6d8a191f0e1721d22ca00afde26b001b34dede5
Sihui Huang <sihuih@gmail.com>
2015-07-11 10:48:07 -0700

    basic gps func

--- Rapeprevention/app/src/main/java/hackmate/rapeprevention/Models/SMS.java
+++ Rapeprevention/app/src/main/java/hackmate/rapeprevention/Models/SMS.java
@@ -1,10 +1,16 @@
 package hackmate.rapeprevention.Models;
 
+import android.content.Context;
 import android.telephony.SmsManager;
 import android.widget.Toast;
 
 public class SMS {
-    public static boolean sendSMS(String number, String text){
+    public static boolean sendSMS(String number, String text, Context context){
+        final GPSTracker  mGPS = new GPSTracker(context);
+        if(mGPS.canGetLocation ){
+            mGPS.getLocation();
+            text+="\n Location : ( "+mGPS.getLatitude()+" , "+mGPS.getLongitude()+" )";
+        }
         try {
             SmsManager smsManager = SmsManager.getDefault();
             smsManager.sendTextMessage(number, null, text, null, null);
