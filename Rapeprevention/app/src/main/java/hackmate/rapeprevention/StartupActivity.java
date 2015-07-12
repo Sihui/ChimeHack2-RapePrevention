def668226ad4e6ee95a5047b1a2ab3f4d680137c
Sihui Huang <sihuih@gmail.com>
2015-07-11 20:00:56 -0700

    add notification code template

--- Rapeprevention/app/src/main/java/hackmate/rapeprevention/StartupActivity.java
+++ Rapeprevention/app/src/main/java/hackmate/rapeprevention/StartupActivity.java
@@ -1,9 +1,14 @@
 package hackmate.rapeprevention;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
@@ -52,15 +57,16 @@ public class StartupActivity extends ActionBarActivity {
 
   @OnClick(R.id.go_to_reaction) void onGotoBtnClick() {
       /*final Handler handler = new Handler();
-      final long intervalTime = 1000;
+      final long intervalTime = 3000;
       handler.postDelayed(new Runnable() {
           @Override
           public void run() {
               System.out.println("What's up");
+              sendNotification();
               handler.postDelayed(this, intervalTime);
           }
-      }, intervalTime);
-      SMS.sendSMS("18329752606", "haha" + GPSTracker.getAddress(this), this);*/
+      }, intervalTime);*/
+      /*SMS.sendSMS("18329752606", "haha" + GPSTracker.getAddress(this), this);*/
     startActivity(new Intent(this, ReactionActivity.class));
   }
 
@@ -82,4 +88,33 @@ public class StartupActivity extends ActionBarActivity {
 
     return super.onOptionsItemSelected(item);
   }
+    private void sendNotification(){
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.circle_green)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!");
+// Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, ReactionActivity.class);
+
+// The stack builder object will contain an artificial back stack for the
+// started Activity.
+// This ensures that navigating backward from the Activity leads out of
+// your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+// Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(ReactionActivity.class);
+// Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+// mId allows you to update the notification later on.
+        mNotificationManager.notify(0, mBuilder.build());
+    }
 }
